bdf23a2ed962ad92ec82e777e61d738deaa45842
cotsom <gerfy.17@mail.ru>
2022-08-16 16:11:55 +0300

    add ws, effects

--- virtuino_mqtt_HiveMQ_private_esp/virtuino_mqtt_HiveMQ_private_esp.ino
+++ virtuino_mqtt_HiveMQ_private_esp/virtuino_mqtt_HiveMQ_private_esp.ino
@@ -1,5 +1,22 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <FastLED.h>
+
+//----------------------Led--------------------------------------------
+// How many leds in your strip?
+#define NUM_LEDS 20
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 2
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+//---------------------------------------------------------------------
+
+int rainbowFlag = 0;
+int blinkFlag = 0;
 
 // WiFi
 const char *ssid = "Potee_2G"; // Enter your WiFi name
@@ -16,7 +33,11 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
+  //Led
   pinMode(LED_BUILTIN, OUTPUT);
+  FastLED.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
+  FastLED.setBrightness(84);
+  
  // Set software serial baud to 115200;
  Serial.begin(115200);
  // connecting to a WiFi network
@@ -46,7 +67,11 @@ void setup() {
  client.subscribe(topic);
 }
 
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
+
 void callback(char *topic, byte *payload, unsigned int length) {
+ static uint8_t hue = 0; 
  Serial.print("Message arrived in topic: ");
  Serial.println(topic);
  Serial.print("Message:");
@@ -56,21 +81,103 @@ void callback(char *topic, byte *payload, unsigned int length) {
      Serial.print((char) payload[i]);
      _message.concat((char) payload[i]);
  }
- if(_message == "on"){
-  digitalWrite(LED_BUILTIN, HIGH);
- }else{
-  digitalWrite(LED_BUILTIN, LOW);
+ 
+ if(_message == "green"){
+  rainbowFlag = 0;
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(255, 255, 255);
+  }
+    FastLED.show(); 
+
+ }else if(_message == "purple"){
+  rainbowFlag = 0;
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(128, 255, 255);
+  }
+    FastLED.show(); 
+
+ }else if(_message == "blue"){
+  rainbowFlag = 0;
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(160, 255, 255);
+  }
+    FastLED.show(); 
+
+ }else if(_message == "white"){
+  rainbowFlag = 0;
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(100, 0, 255);
+  }
+    FastLED.show(); 
+
+ }else if(_message == "rainbow"){
+  rainbowFlag = 1;
+ }else if(_message == "stop"){
+  rainbowFlag = 0;
+ }else if(_message == "blink"){
+  blinkFlag = 1;
  }
+
+ 
  Serial.println();
  Serial.println("-----------------------");
 }
 
 void loop() {
  client.loop();
- client.publish(topic, "on");
- delay(1500);
+ //client.publish(topic, "on");
+ //delay(1500);
 
- client.publish(topic, "off");
- delay(1500);
+ //client.publish(topic, "off");
+ //delay(1500);
+ if (rainbowFlag){
+  static uint8_t hue = 0;
+    // First slide the led in one direction
+    for(int i = 0; i < NUM_LEDS; i++) {
+      // Set the i'th led to red 
+      leds[i] = CHSV(hue++, 255, 255);
+      leds[i];
+      // Show the leds
+      FastLED.show(); 
+      // now that we've shown the leds, reset the i'th led to black
+      // leds[i] = CRGB::Black;
+      fadeall();
+      // Wait a little bit before we loop around and do it again
+      delay(50);
+    }
+  
+    // Now go in the other direction.  
+    for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+      // Set the i'th led to red 
+      leds[i] = CHSV(hue++, 255, 255);
+      // Show the leds
+      FastLED.show();
+      // now that we've shown the leds, reset the i'th led to black
+      // leds[i] = CRGB::Black;
+      fadeall();
+      // Wait a little bit before we loop around and do it again
+      delay(50);
+    }
+  //flag = 0;
+ }
+
+ if (blinkFlag){
+  for(int i = 0; i < 2; i++){
+    for(int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV(0, 0, 0);
+    }
+    FastLED.show();
+    fadeall();
+    delay(110);
+
+    for(int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV(255, 255, 255);
+    }
+    FastLED.show();
+    fadeall();
+    delay(110);
+  }
+  blinkFlag = 0;
+ }
  
 }
